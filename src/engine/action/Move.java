6bc95018d1e78836f910261bc57728f307ef7f22
Aidan Wilkinson <ajw853@cs.bham.ac.uk>
2019-06-30 17:07:05 +0100

    getter for Move class

--- src/engine/action/Move.java
+++ src/engine/action/Move.java
@@ -3,13 +3,19 @@ package engine.action;
 import engine.board.Tile;
 
 public class Move extends Action {
-	Tile destination;
+	private Tile destination;
 	
 	
 
 	public Move(Tile sourceTile, Tile destination) {
 		super(sourceTile);
 		this.destination = destination;
+	}
+
+
+
+	public Tile getDestination() {
+		return destination;
 	}
 
 
